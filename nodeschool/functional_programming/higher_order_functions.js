a3435705d18fa9dfb2eea10b078eceb7c96fa64d
Roberto Lewis <roberto.lewis@gmail.com>
2016-01-24 14:40:38 -0800

    initial commit

---
+++ nodeschool/functional_programming/higher_order_functions.js
@@ -0,0 +1,12 @@
+// Implement a function that takes a function as its first argument, a number num as its second argument, then executes the passed in function num times.
+//
+// Use the boilerplate code given to you below to get started. Most/all future exercises will provide boilerplate.
+
+function repeat(operation, num) {
+     for (var i = 0; i <= num; i++){
+        operation();
+     }
+   }
+
+   // Do not remove the line below
+   module.exports = repeat
