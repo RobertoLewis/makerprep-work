4911e3e8bcf7c3e81e1f158cd96cad7ed74fb8b6
Roberto Lewis <roberto.lewis@gmail.com>
2016-02-12 11:38:35 -0800

    initial commit

---
+++ nodeschool/scope-chains-closures/scopes.js
@@ -0,0 +1,9 @@
+//
+// In an empty file, create a function foo which contains one variable lexically
+// scoped named bar.
+//
+// Once complete, execute scope-chains-closures verify <your-file.js> to verify your solution.
+
+function foo () {
+  var bar;
+}
